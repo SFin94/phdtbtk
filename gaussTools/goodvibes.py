320356054b27271e9b57bedfbbca302d051ce25f
Sophie <smf115@ic.ac.uk>
2020-08-18 16:36:04 +0100

    added main workflow routine to call

--- gaussTools/goodvibes.py
+++ gaussTools/goodvibes.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.preprocessing import LabelEncoder
+
+import phdtbtk
 import molLego as ml
-import phdtbtk.baseTools.dataframe_funcs as dfuncs
 
 
 '''Script to process GoodVibes results and to add quasi-harmonic values to an existing dataframe of molecules'''
@@ -89,7 +90,7 @@ def process_dataframes(goodvibes_index, mol_data):
 
 def check_thermo_results(mol_data, goodvibes_data, calculation_properties):
 
-    '''Checks that RRHO thermodynamic quantities are the same across the goodvibes and gaussian thermochemistry results. If not then prints a warning.
+    '''Checks that RRHO thermodynamic quantities are the same across the goodvibes and gaussian thermochemistry results. If not then prints a warning. Copies of the DataFrames are created to avoid changing the data in place.
 
     Only the electronic E, H, ZPE are checked as TS and G will not match if solvation calculation
 
@@ -97,11 +98,12 @@ def check_thermo_results(mol_data, goodvibes_data, calculation_properties):
      mol_data: pd DataFrame - dataframe of molecule data from gaussian log file
      goodvibes_data: pd DataFrame - dataframe containing the results from a goodvibes calculations
      calculation_properties: Dict - properties of the GoodVibes calculation including concentration, temperature and scale factor
-    ''' 
 
+    ''' 
     # Convert all quantities to kj/mol
-    goodvibes_data[:] *= 2625.5
-    mol_data['E SCF (h)'] *= 2625.5
+    goodvibes_comp_data = goodvibes_data[:]*2625.5
+    mol_comp_data = mol_data
+    mol_comp_data['E SCF (h)'] *= 2625.5
 
     mol_cols = ['ZPE', 'E SCF (h)', 'H', 'G']
     goodvibes_cols = ['ZPE', 'E', 'H', 'G(T)']
@@ -109,17 +111,17 @@ def check_thermo_results(mol_data, goodvibes_data, calculation_properties):
     diff = pd.DataFrame()
     
     # Try comparison with unscaled gaussian data
-    comp = dfuncs.data_comp(mol_data, goodvibes_data, df_cols_1=mol_cols, df_cols_2=goodvibes_cols, tol=1e-2)
+    comp = phdtbtk.data_comp(mol_comp_data, goodvibes_comp_data, df_cols_1=mol_cols, df_cols_2=goodvibes_cols, tol=1e-2)
     if all(comp):
         return
     
-    # Try comaprison with scaled gaussian data
+    # Try comparison with scaled gaussian data
     elif ('scale_factor' in calculation_properties):
         scale_factor = calculation_properties['scale_factor']
-        mol_data['ZPE_scaled'] = scale_factor*mol_data['ZPE']
-        mol_data['H_scaled'] = mol_data['H'] - mol_data['ZPE'] + mol_data['ZPE_scaled']
+        mol_comp_data['ZPE_scaled'] = scale_factor*mol_comp_data['ZPE']
+        mol_comp_data['H_scaled'] = mol_comp_data['H'] - mol_comp_data['ZPE'] + mol_comp_data['ZPE_scaled']
         mol_cols = ['ZPE_scaled', 'E SCF (h)', 'H_scaled']
-        comp = dfuncs.data_comp(mol_data, goodvibes_data, df_cols_1=mol_cols, df_cols_2=goodvibes_cols[:-1], tol=1e-2)
+        comp = phdtbtk.data_comp(mol_comp_data, goodvibes_comp_data, df_cols_1=mol_cols, df_cols_2=goodvibes_cols[:-1], tol=1e-2)
 
     if not all(comp):
         print('Warning: goodvibes thermo results do not match original gaussian results (tolerence: ', 1e-2, ')')
@@ -153,24 +155,21 @@ def append_goodvibes(mol_data, goodvibes_data):
     return mol_data
 
 
-if __name__ == "__main__":
-    
-    '''Parse in the input log files of the scan calculations and any additional input file containing tracked parameters.
+def process_goodvibes(molecule_file, goodvibes_file, new_file=False, save=None):
+
     '''
+    Combined workflow for processing gaussian molecule data, goodvibes data, and appending the two
+    
+    Parameters:
+     molecule_data: str - name/location of file for molecule data [csv or conf]
+     goodvibes_data: str - name/location of goodvibes data file
+     new_file: bool - Flag of whether results should be appended to the existing molecule data file or create a new file
+     save: str - new file name to write appended data too [default: None]
 
-    usage = "usage: %(prog)s [goodvibes_data] [mol_data]"
-    parser = argparse.ArgumentParser(usage=usage)
-
-    parser.add_argument("goodvibes_file", type=str, help="Goodvibes results file")
-    parser.add_argument("molecule_data", type=str, help="Existing datafile (.csv) of molecule data for goodvibes results to be appended too")
-    parser.add_argument("-n", "--new", dest="new_file", type=bool, default=False, help="Flag of whether a new file should be created with the results [default] or append to the old file")
-    parser.add_argument("-s", "--save", dest="save", type=str, help="Name of file to save new results too (minus .csv extension)")
-    args = parser.parse_args()
-    save = args.save
-
+    '''
     # Create initial dataframes for existing molecule results and goodvibes results
-    mol_data_initial = pd.read_csv(args.molecule_data)
-    gv_data, gv_calc_properties = parse_goodvibes(args.goodvibes_file)
+    mol_data_initial, mols = phdtbtk.process_input_file(molecule_file)
+    gv_data, gv_calc_properties = parse_goodvibes(goodvibes_file)
     
     # Process mol dataframe to have shared index and order with goodvibes df
     mol_data = process_dataframes(gv_data.index, mol_data_initial)
@@ -180,14 +179,39 @@ if __name__ == "__main__":
 
     # Add columns to dataframe
     mol_data_full = append_goodvibes(mol_data, gv_data)
-
-    # Save file as new file or rewrite old one
-    if args.new_file == False:
+    
+    # Save as new file or rewrite old one
+    if new_file == False:
         if save == None:
-            save = args.molecule_data.split('.')[0] + '_goodvibes'
+            save = molecule_file.split('.')[0] + '_goodvibes'
         mol_data_full.to_csv(save + '.csv')    
     else:
-        mol_data_full.to_csv(args.molecule_data)
+        mol_data_full.to_csv(molecule_file)
+
+
+if __name__ == "__main__":
+    
+    '''
+    CL arguments for running goodvibes analysis/appending dataframe.
+    '''
+
+    usage = "usage: %(prog)s [goodvibes_data] [mol_data]"
+    parser = argparse.ArgumentParser(usage=usage)
+
+    parser.add_argument("goodvibes_file", type=str, 
+                        help="Goodvibes results file")
+    parser.add_argument("molecule_data", type=str, 
+                        help="Existing datafile (.csv) of molecule data or conf file ofmolecules to append goodvibes results to be appended too")
+    parser.add_argument("-n", "--new", dest="new_file", action='store_false', 
+                        help="Flag of whether a new file should be created with the results [default] or append to the existing molecule_data file")
+    parser.add_argument("-s", "--save", dest="save", type=str, 
+                        help="Name of file to save new results too (minus .csv extension)")
+    args = parser.parse_args()
+    
+    # Call process workflow to add goodvibes data to existing molecules/molecule dataframe
+    process_goodvibes(args.molecule_file, args.goodvibes_file, new_file=args.new_file, save=args.save)
+
+
 
 
 
